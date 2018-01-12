acaf725e68e75b86a14cfef5ac534f710c1d0e80
William Blake <willblakebooking@gmail.com>
2018-01-12 11:43:01 -0600

    added switch events

--- liri.js
+++ liri.js
@@ -4,53 +4,66 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 
-var userSearch = process.argv;
+var userCommand = process.argv[2];
+var userQuery = process.argv[3];
 
-var songName = "";
+switch(userCommand) {
+    case('spotify-this-song'):
+    var trackSearch = userQuery;
 
-for (i = 2; i < userSearch.length; i++) {
+    var songName = "";
 
-  songName = songName + " " + userSearch[i];
+    for (i = 2; i < trackSearch.length; i++) {
 
+      songName = songName + " " + trackSearch[i];
+
+    }
+      // Begin Spotify API call
+      var spotify = new Spotify({
+        id: process.env.SPOTIFY_ID,
+        secret: process.env.SPOTIFY_SECRET
+      });
+
+      spotify.search({ type: 'track', query: songName, limit: 1 }, function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+
+        console.log(JSON.stringify(data, null, 2));
+      });
+      //End Spotify API call
+        break;
+
+    case('my-tweets'):
+        // Begin Twitter API call
+        var client = new Twitter({
+          consumer_key: process.env.TWITTER_CONSUMER_KEY,
+          consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+          access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+          access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        });
+
+        var params = {totes_gnar: 'nodejs'};
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+          if (!error) {
+            console.log(tweets);
+          }
+        });
+        // End Twitter API call
+        break;
+
+      case('movie-this'):
+        // Begin omdb API call
+        request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+          if (!error && response.statusCode === 200) {
+
+            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+          }
+        });
+        // End omdb API call
+        break;
+      default:
+        console.log('failure')
+        break;
 }
-
-// Begin Spotify API call
-var spotify = new Spotify({
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-});
-
-spotify.search({ type: 'track', query: songName, limit: 1 }, function (err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-
-  console.log(JSON.stringify(data, null, 2));
-});
-//End Spotify API call
-
-// Begin Twitter API call
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-
-var params = {totes_gnar: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-// End Twitter API call
-
-// Begin omdb API call
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-  if (!error && response.statusCode === 200) {
-
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  }
-});
-// End omdb API call
