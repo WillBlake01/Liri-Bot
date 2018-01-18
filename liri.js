2c765ba525697af2b8ce3099f0f817c384a832b2
William Blake <willblakebooking@gmail.com>
2018-01-17 18:20:38 -0600

    added error to twitter call

--- liri.js
+++ liri.js
@@ -44,7 +44,9 @@ switch (userCommand) {
     var params = { totes_gnar: 'nodejs' };
 
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
-      if (!error) {
+      if (error) {
+        return console.log(error);
+      } else if (!error) {
         for (var i = 0; i < tweets.length; i++) {
           console.log(tweets[i].text);
           console.log(tweets[i].created_at);
